b419114dac738132451926e31f303cf3eb6eb2e1
Saloni Bhakte <salonibhakte@gmail.com>
2026-03-22 01:09:10 +0530

    Initial commit

---
+++ Person.java
@@ -0,0 +1,22 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Saloni", 20);
+        Person p2 = new Person("Rahul", 22);
+
+        p1.display();
+        p2.display();
+    }
+}
