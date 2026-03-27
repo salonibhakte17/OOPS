9ac9ab93744c96e7ba3a74a08f1405b18412e56e
Saloni Bhakte <salonibhakte@gmail.com>
2026-03-27 21:13:36 +0530

    Question-8(U3)

---
+++ TextProcessingBuilder.java
@@ -0,0 +1,11 @@
+public class TextProcessingBuilder {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Welcome");
+
+        sb.append(" to Java Programming");
+
+        System.out.println("Modified string: " + sb);
+
+    }
+}
