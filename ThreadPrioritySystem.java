1cd276bee98431a1fd9dd87468e28c862ae1330e
Saloni Bhakte <salonibhakte@gmail.com>
2026-03-30 22:13:15 +0530

    Question-20(U4)

---
+++ ThreadPrioritySystem.java
@@ -0,0 +1,26 @@
+class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public void run() {
+        System.out.println("Running: " + getName() + 
+                           " | Priority: " + getPriority());
+    }
+}
+
+public class ThreadPrioritySystem {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread-1");
+        MyThread t2 = new MyThread("Thread-2");
+        MyThread t3 = new MyThread("Thread-3");
+
+        t1.setPriority(Thread.MIN_PRIORITY);   // 1
+        t2.setPriority(Thread.NORM_PRIORITY);  // 5
+        t3.setPriority(Thread.MAX_PRIORITY);   // 10
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
